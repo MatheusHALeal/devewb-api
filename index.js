ab4e6854c5f2ce9a8e4af0ba1d982a41c2b2e366
MatheusHALeal <matheus.leal@ccc.ufcg.edu.br>
2018-08-26 14:37:32 -0300

    tutorial

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require('express')
+const app = express()
+
+app.get('/', (req, res) => res.send('Hello World!'))
+
+app.listen(3000, () => console.log('Example app listening on port 3000!'))
+
+app.get('/get', function(req, res) {
+    res.send('Eai put*** do ca*****');
+});
+
+app.post('/post', function(req, res) {
+    res.send('Post request');
+});
+
+app.put('/put', function(req, res) {
+    res.send('Put request');
+});
+
+app.delete('/delete', function(req, res) {
+    res.send('Delete request');
+});
