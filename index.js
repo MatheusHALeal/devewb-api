10082687e180df2abe437d24af41713a5236bbf7
MatheusHALeal <matheus.leal@ccc.ufcg.edu.br>
2018-08-29 10:09:09 -0300

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ app.get('/', (req, res) => res.send('Hello World!'))
 app.listen(3000, () => console.log('Example app listening on port 3000!'))
 
 app.get('/get', function(req, res) {
-    res.send('Eai put*** do ca*****');
+    res.send('Olá');
 });
 
 app.post('/post', function(req, res) {
